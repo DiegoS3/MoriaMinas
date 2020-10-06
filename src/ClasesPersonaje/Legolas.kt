58fe991d5fe3eb9f4f1ddc18a0c2baf4fc3cdd45
DiegoS3 <diegozj92@gmail.com>
2020-10-06 21:18:42 +0200

    Metodos legolas

--- src/ClasesPersonaje/Legolas.kt
+++ src/ClasesPersonaje/Legolas.kt
@@ -2,7 +2,15 @@ package ClasesPersonaje
 
 class Legolas (var carcaj : Int, nombre : String, vivo : Boolean) : Personaje(nombre, vivo){
 
-    private fun lanzarFlecha(){}
+    private fun lanzarFlecha(){
+
+        if(this.carcaj > 0){
+
+            this.carcaj --
+
+        }
+
+    }
 
     private fun recargarCarcaj(flechas : Int){}
 
