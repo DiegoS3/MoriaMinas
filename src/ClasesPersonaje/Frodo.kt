77181e8e175e10037feca418b51e593876060d51
DiegoS3 <diegozj92@gmail.com>
2020-10-06 20:43:36 +0200

    Frodo estructura

---
+++ src/ClasesPersonaje/Frodo.kt
@@ -0,0 +1,9 @@
+package ClasesPersonaje
+
+class Frodo (var anillo : Boolean, nombre : String, vivo : Boolean) : Personaje(nombre, vivo){
+
+    private fun ponerseAnillo(){}
+
+    private fun quitarseAnillo(){}
+
+}
