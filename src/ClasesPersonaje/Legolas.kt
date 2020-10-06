3a20c095951ee16d61dc43680b0ad94214322e43
DiegoS3 <diegozj92@gmail.com>
2020-10-06 20:38:40 +0200

    Legolas estructura

---
+++ src/ClasesPersonaje/Legolas.kt
@@ -0,0 +1,9 @@
+package ClasesPersonaje
+
+class Legolas (var carcaj : Int, nombre : String, vivo : Boolean) : Personaje(nombre, vivo){
+
+    private fun lanzarFlecha(){}
+
+    private fun recargarCarcaj(flechas : Int){}
+
+}
