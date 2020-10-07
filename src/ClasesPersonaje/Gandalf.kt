cc05412d0d8bf8b9e14679a65595d205333bbed0
DiegoS3 <diegozj92@gmail.com>
2020-10-07 18:32:40 +0200

    Crear Metodos Gandalf

--- src/ClasesPersonaje/Gandalf.kt
+++ src/ClasesPersonaje/Gandalf.kt
@@ -2,11 +2,17 @@ package ClasesPersonaje
 
 class Gandalf(var vara : Int, nombre : String, vivo : Boolean) : Personaje(nombre, vivo) {
 
-    private fun recargarVara(energia: Int){}
+    private fun recargarVara(energia: Int){
+
+        vara += energia
+
+        if (vara > Constantes.MAXVARAGANDALF) vara = Constantes.MAXVARAGANDALF
+
+    }
 
     private fun poderVara() : Int{
 
-        return this.vara
+        return vara
 
     }
 
